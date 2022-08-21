7e4072402b0973b3a881e8ef2c4afaf814be88b6
Mahmudul Islam <mahmudulislam14270@gmail.com>
2022-08-21 22:58:14 +0600

    jwt add

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion,ObjectId  } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
@@ -10,7 +11,22 @@ app.use(cors());
 app.use(express.json());
 
 
+// jwt token
 
+function verifyJET(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+        return res.status(401).send({ message : 'Invalid authorization'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded) => {
+        if(err) {
+            return res.status(403).send({ message :'Forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wv0xnnl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -19,6 +35,18 @@ async function run(){
     try {
         await client.connect();
         const productCollection = client.db('productCollection').collection('products');
+        const orderCollection = client.db('productCollection').collection('orders');
+
+        // AUTH jwt token 
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+
 
         // get data 
         app.get('/products',async(req, res) => {
@@ -64,7 +92,31 @@ async function run(){
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
+        });
+
+        // post order 
+        app.post('/orders', async(req, res)=>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+
+        });
+
+        // get oder product
+        app.get('/orders',verifyJET, async(req, res)=>{
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = {email:email};
+                const cursor =  orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else{
+                res.status(403).send({message: 'forbidden access'})
+            }
         })
+
     }
    finally {
 
@@ -76,8 +128,6 @@ run().catch(console.dir);
 app.get('/', (req, res) =>{
     res.send('Server is running')
 });
-
-
 app.listen(port,()=>{
     console.log('listening on port',port);
 });
